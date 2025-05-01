e829b41efd0c69ce7e0f7bbbe21ebdacc1711946
Kika001 <myjmst1@gmaile.com>
2025-05-01 18:26:42 +0200

    03: home layout

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <footer className="flex border-t justify-betwwen font-medium p-6">
+            <div className="flex items-center gap-2">
+                <p>Ulearn</p>
+            </div>
+        </footer>
+    )
+}
